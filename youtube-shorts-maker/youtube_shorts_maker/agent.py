e41c7c1cf452884963daf011ff1926a667faa433
minki <alsrl0023@naver.com>
2026-07-20 17:50:36 +0900

    callback

--- youtube-shorts-maker/youtube_shorts_maker/agent.py
+++ youtube-shorts-maker/youtube_shorts_maker/agent.py
@@ -1,6 +1,10 @@
 from google.adk.agents import Agent
+from google.adk.agents.callback_context import CallbackContext
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.models.llm_request import LlmRequest
+from google.adk.models.llm_response import LlmResponse
 from google.adk.tools.agent_tool import AgentTool
+from google.genai import types
 
 from .prompt import SHORTS_PRODUCER_DESCRIPTION, SHORTS_PRODUCER_PROMPT
 from .sub_agents.asset_generator.agent import asset_generator_agent
@@ -8,6 +12,31 @@ from .sub_agents.content_planner.agent import content_planner_agent
 from .sub_agents.video_assembler.agent import video_assembler_agent
 
 MODEL = LiteLlm(model="openai/gpt-4o")
+
+
+def before_model_callback(
+    callback_context: CallbackContext, llm_request: LlmRequest
+) -> LlmResponse:
+
+    history = llm_request.contents
+    last_message = history[:-1]
+
+    if last_message.role == "user":
+        text = last_message.parts[0].text
+        if "hummus" in text:
+            return LlmResponse(
+                content=types.Content(
+                    parts=[
+                        types.Part(
+                            text="Sorry I can't help with that",
+                        )
+                    ],
+                    role="model",
+                )
+            )
+
+    return None
+
 
 shorts_producer_agent = Agent(
     name="ShortsProducerAgent",
@@ -19,6 +48,7 @@ shorts_producer_agent = Agent(
         AgentTool(agent=asset_generator_agent),
         AgentTool(agent=video_assembler_agent),
     ],
+    before_model_callback=before_model_callback,
 )
 
 root_agent = shorts_producer_agent
