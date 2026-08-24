eb89eb6eba35f6197a903614fb04dab2c819628d
minki <alsrl0023@naver.com>
2026-08-24 17:35:38 +0900

    a2a fin

--- a2a/user-facing-agent/user_facing_agent/agent.py
+++ a2a/user-facing-agent/user_facing_agent/agent.py
@@ -15,11 +15,18 @@ history_agent = RemoteA2aAgent(
     agent_card=f"http://127.0.0.1:8001{AGENT_CARD_WELL_KNOWN_PATH}",
 )
 
+philosophy_agent = RemoteA2aAgent(
+    name="PhilosophyHelperAgent",
+    description="An agent that can help students with their philosophy homework",
+    agent_card=f"http://127.0.0.1:8002{AGENT_CARD_WELL_KNOWN_PATH}",
+)
+
 root_agent = Agent(
     name="StudentHelperAgent",
     description="An agent that can helps students with their homework",
     model=LiteLlm(model="openai/gpt-4o-mini"),
     sub_agents=[
         history_agent,
+        philosophy_agent,
     ],
 )
